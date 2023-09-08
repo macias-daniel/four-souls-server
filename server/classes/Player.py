bbe9aaf72e0db3c3b8123914cc0c927a62e31d05
BM-Becerra <duddy3000@gmail.com>
2023-09-08 11:47:55 -0700

    Made base for the server connection

--- server/classes/Player.py
+++ server/classes/Player.py
@@ -2,12 +2,19 @@ from classes.Character import Character
 
 
 class Player:
-    def __init__(self, name, character) -> None:
-        self.name = name
-        self.character = character
+    def __init__(self, ID) -> None:
+        self.ID = ID
+
+    #    self.character = character
 
     def getName(self):
         return self.name
 
     def getCharacter(self):
         return self.character
+
+    def setName(self, name):
+        self.name = name
+
+    def setCharacter(self, character):
+        self.character = character
--- server/main.py
+++ server/main.py
@@ -1,10 +1,52 @@
 from classes.Card import Card
 from classes.Player import Player
+import socket
+from _thread import *
+import sys
 
-# card_1 = Card("First", "This is the first card we have")
-playerNum = input("Enter number of players: ")
-print(playerNum)
-player1 = Player("Bob", "Isaac")
+server = ""
+# safe port to connect to
+port = 5555
+# Types of connection. Connection to an IPV4 address.
+# Stream represents how server string comes in.
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# print(card_1.name)
-print(player1.getName())
+# Bind the server and port to the socket. If the port is being used, it fails.
+# Setting up a port to look for certain connections.
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+# If left blank, it allows for unlimited connections to happen.
+# The param indicates how many people can listen.
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+
+# When connecting to a client, we want this to continually run.
+def threaded_client(conn):
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048 * 4)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending: ", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+
+# Continually listen for connections.
+while True:
+    conn, addr = s.accept()
+    print("Connected to: ", addr)
+
+    start_new_thread(threaded_client, (conn,))
